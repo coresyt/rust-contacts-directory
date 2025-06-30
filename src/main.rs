82d4a85b0080f11f408d2b545d5d55d0e99c2ef0
coresyt <corejsyt2011@gmail.com>
2025-06-29 19:15:26 -0600

    feat: Add functionality search in contacts directory

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod utils;
 
+use crate::utils::json::IContact;
 use std::io::stdin;
 use std::io::{Write, stdout};
 use std::{thread, time};
 
 fn main() {
-    print!("\x1B[2J\x1B[H");
     loop {
-        // let _ = json::read_file();
+        print!("\x1B[2J\x1B[H");
         const OPTIONS: [&'static str; 5] = [
             "Search Contact",
             "Add Contact",
@@ -16,18 +16,18 @@ fn main() {
             "Exit",
         ];
         let mut option_index: String = String::new();
-        let contacts: Vec<utils::json::IContact> = vec![
-            utils::json::IContact {
+        let contacts: Vec<IContact> = vec![
+            IContact {
                 first_name: "Pedro",
                 last_name: "Diaz",
                 phone_number: 523327853326,
             },
-            utils::json::IContact {
+            IContact {
                 first_name: "Juan",
                 last_name: "Pérez",
                 phone_number: 5512345678,
             },
-            utils::json::IContact {
+            IContact {
                 first_name: "Luis",
                 last_name: "Hernández",
                 phone_number: 5534567890,
@@ -38,12 +38,16 @@ fn main() {
 
         println!("Hello! This is the contact list.");
 
-        utils::welcome::welcome(contacts_length, contacts, OPTIONS, OPTIONS_LENGTH);
+        utils::welcome::welcome(
+            contacts_length,
+            (&contacts).to_vec(),
+            OPTIONS,
+            OPTIONS_LENGTH,
+        );
 
         print!("What do you want to do now? ");
         stdout().flush().unwrap();
         let _ = stdin().read_line(&mut option_index);
-        println!();
 
         let option_index_int = match option_index.trim().parse() {
             Ok(c) => c,
@@ -59,7 +63,36 @@ fn main() {
         //     ? _ -------> Retry Question
         match option_index_int {
             1 => {
-                println!("Searching...")
+                let mut search: Vec<IContact> = Vec::new();
+                let mut key_to_search = String::new();
+                print!("Search => ");
+                stdout().flush().unwrap();
+                let _ = stdin().read_line(&mut key_to_search);
+
+                for i in 0..contacts_length {
+                    let contact = &contacts[i];
+
+                    let include_first_name = contact.first_name.contains(&key_to_search.trim());
+                    let include_last_name = contact.last_name.contains(&key_to_search.trim());
+                    let include_phone_number = contact
+                        .phone_number
+                        .to_string()
+                        .contains(&key_to_search.trim());
+
+                    if include_first_name == true
+                        || include_last_name == true
+                        || include_phone_number == true
+                    {
+                        search.push(contact.clone());
+                    }
+                    continue;
+                }
+                utils::show_contacts(search.len(), (&search).to_vec());
+                for searched in search {
+                    println!("{}", searched.first_name)
+                }
+                println!();
+                thread::sleep(time::Duration::from_secs(10));
             }
             2 => {
                 println!("Creating...")
@@ -75,11 +108,11 @@ fn main() {
                 print!("1...");
                 stdout().flush().unwrap();
                 thread::sleep(time::Duration::from_secs(1));
-                
+
                 print!("2...");
                 stdout().flush().unwrap();
                 thread::sleep(time::Duration::from_secs(1));
-                
+
                 print!("3...");
                 stdout().flush().unwrap();
                 thread::sleep(time::Duration::from_secs(1));

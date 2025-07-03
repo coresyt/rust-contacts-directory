bf629e6bd100d981d39e644aae64d3c92aff98e9
coresyt <corejsyt2011@gmail.com>
2025-07-02 20:19:00 -0600

    [feat,fix]: Updating the utils::json::get_contacts function and changing the utils::json::IContact struct

--- src/utils/json.rs
+++ src/utils/json.rs
@@ -2,30 +2,23 @@ use serde;
 use serde_json;
 use std::fs;
 
-#[derive(Clone, Copy, serde::Serialize, serde::Deserialize, Debug)]
-pub struct IContact<'a> {
-    pub first_name: &'a str,
-    pub last_name: &'a str,
+#[derive(Clone, serde::Serialize, serde::Deserialize, Debug)]
+pub struct IContact {
+    pub first_name: String,
+    pub last_name: String,
     pub phone_number: u64,
 }
 
-pub fn read_file() -> Result<(), Box<dyn std::error::Error>> {
-    let contenido = match fs::read_to_string("personas.json") {
+pub fn get_contacts() -> Vec<IContact> {
+    let content = match fs::read_to_string("personas.json") {
         Ok(c) => c,
-        Err(e) => {
-            eprintln!("Error de I/O: {}", e);
-            return Err(Box::new(e));
-        }
+        Err(_e) => String::new()
     };
 
-    match serde_json::from_str::<Vec<IContact>>(&contenido) {
-        Ok(c) => {
-            println!("{:?}", c);
-        }
-        Err(e) => {
-            eprintln!("Error de I/O: {}", e);
-            return Err(Box::new(e));
-        }
+    let contacts : Vec<IContact> = match serde_json::from_str::<Vec<IContact>>(content.as_str()) {
+        Ok(c) => c,
+        Err(_e) => vec![]
     };
-    Ok(())
+
+    return contacts;
 }
